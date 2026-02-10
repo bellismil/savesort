b6fb3bac39372ed74af5bb5a7484ce60ed450005
bellismil <auinco25@gmail.com>
2026-02-10 12:58:49 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
-        '/savesort-mvp.html',
+        '/index.html',
         '/manifest.json'
       ]);
     })
